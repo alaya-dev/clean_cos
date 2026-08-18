0f3ecc44bc025c09be7dd55e28067455f299d8ef
Hajjej-adam <ademhajjej2002@gmail.com>
2026-08-18 18:18:33 +0100

    fix pagintaion

--- app/Http/Controllers/StorefrontCatalogController.php
+++ app/Http/Controllers/StorefrontCatalogController.php
@@ -52,7 +52,6 @@ class StorefrontCatalogController extends Controller
         $products = $this->applyFilters($this->catalogueQuery(), $request);
         $this->applyDefaultCatalogueOrder($products, $request);
         $products = $products->paginate(20)->withQueryString();
-        $products->load('category.parent');
         $productsBySubcategory = $products->getCollection()->groupBy('category_id');
 
         return view('storefront.products.index', compact('categories', 'products', 'productsBySubcategory'));
@@ -127,6 +126,7 @@ class StorefrontCatalogController extends Controller
         return Product::public()
             ->with([
                 'category:id,parent_id,name,slug',
+                'category.parent:id,name,slug',
                 'images' => fn ($query) => $query->where('processing_status', 'ready')->orderByDesc('is_primary')->orderBy('sort_order'),
             ]);
     }
